6006df74db01ff34523743a2500e25fb62097981
don16377 <157607835+don16377@users.noreply.github.com>
2024-01-24 14:43:58 +0100

    commit

--- snap.py
+++ snap.py
@@ -46,7 +46,7 @@ d1mod = input (red_color + '''
 
 
 if d1mod == "1":
-  user = input("USERNAME OF SNAPCHAT => ")
+  user = input("USERNAME OF SNAPCHAT =>l3aah136 ")
   flo = input("FILE PASSWORD (COMBO) => ")
 
   url = "https://gcp.api.snapchat.com/scauth/login"
